a4d10e0778e92d2c39b6354aa2865b7041ffd7a8
Hugo Ricardo Gonçalves Martins <rikhard.martins@gmail.com>
2014-12-01 16:28:26 +0000

    Commit inicial.

---
+++ ClubeMGAndroid/app/src/main/java/pt/rikmartins/clubemgandroid/modelos/Noticia.java
@@ -0,0 +1,9 @@
+package pt.rikmartins.clubemgandroid.modelos;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by ricardo on 01-12-2014.
+ */
+public class Noticia extends RealmObject {
+}
---
+++ ClubeMGAndroid/app/src/main/java/pt/rikmartins/clubemgandroid/modelos/Versao.java
@@ -0,0 +1,10 @@
+package pt.rikmartins.clubemgandroid.modelos;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by ricardo on 01-12-2014.
+ */
+public class Versao extends RealmObject{
+    private long versao;
+}
