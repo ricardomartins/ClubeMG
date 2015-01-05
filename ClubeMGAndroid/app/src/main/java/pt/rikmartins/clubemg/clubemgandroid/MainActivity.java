4b3f10b19bc9cd1d6d97867ea2fb39854646b36f
Hugo Ricardo Gonçalves Martins <rikhard.martins@gmail.com>
2015-01-05 14:19:20 +0000

    Resolve #7

--- ClubeMGAndroid/app/src/main/java/pt/rikmartins/clubemg/clubemgandroid/MainActivity.java
+++ ClubeMGAndroid/app/src/main/java/pt/rikmartins/clubemg/clubemgandroid/MainActivity.java
@@ -1,6 +1,5 @@
 package pt.rikmartins.clubemg.clubemgandroid;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
