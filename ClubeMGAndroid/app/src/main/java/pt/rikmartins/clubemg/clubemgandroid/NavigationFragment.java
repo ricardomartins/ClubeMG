6f3608be944ded574dea6ca4546cbab0bb8222f5
Hugo Ricardo Gonçalves Martins <rikhard.martins@gmail.com>
2015-01-22 10:31:09 +0000

    Finalizado a apresentação dos icones das categorias e correcção ortográfica das mesmas. Adicionados os icones de definições e notícias. Removido o layout landscape da actividade.

--- ClubeMGAndroid/app/src/main/java/pt/rikmartins/clubemg/clubemgandroid/NavigationFragment.java
+++ ClubeMGAndroid/app/src/main/java/pt/rikmartins/clubemg/clubemgandroid/NavigationFragment.java
@@ -5,7 +5,10 @@ import android.app.Fragment;
 import android.app.LoaderManager;
 import android.content.CursorLoader;
 import android.content.Loader;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
 import android.database.Cursor;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -14,12 +17,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 import com.commonsware.cwac.merge.MergeAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import pt.rikmartins.clubemg.clubemgandroid.provider.NoticiaContract;
 import pt.rikmartins.clubemg.clubemgandroid.provider.NoticiaProvider;
@@ -42,13 +50,27 @@ public class NavigationFragment
 
     private static final int URL_LOADER_CATEGORIAS = 10;
 
-    private static final String[] coiso_from = new String[]{
+    private static final String[] CATEGORIAS_FROM = new String[]{
+            NoticiaContract.Categoria.COLUMN_NAME_DESIGNACAO,
             NoticiaContract.Categoria.COLUMN_NAME_DESIGNACAO
     };
-    private static final int[] coiso_to = new int[]{
-            R.id.item_navegacao
+
+    private static final String OUTROS_ICON = "icon";
+    private static final String OUTROS_TITULO = "título";
+    private static final String[] OUTROS_FROM = new String[] {
+            OUTROS_ICON,
+            OUTROS_TITULO
     };
+
+    private static final int[] DRAWER_LIST_ITEM_TO = new int[]{
+            R.id.image_view_item_navegacao,
+            R.id.item_navegacao,
+    };
+
+
     private SimpleCursorAdapter mCategoriasCursorAdapter;
+
+    private HashMap<String, DescriptorCategoriaConhecida> descricaoCategoriasConhecidas;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -77,25 +99,54 @@ public class NavigationFragment
 
         mNavigationMergeAdapter = new MergeAdapter();
 
-        ArrayAdapter<String> semFiltroAdapter = new ArrayAdapter<>(getActivity(), R.layout.drawer_list_item, R.id.item_navegacao, new String[]{"Notícias"});
+        ArrayList<HashMap<String, Object>> simpleAdapterData = new ArrayList<>(1);
+        simpleAdapterData.add(new HashMap<String, Object>(2));
+        simpleAdapterData.get(0).put(OUTROS_ICON, R.drawable.ic_dashboard_grey600_24dp);
+        simpleAdapterData.get(0).put(OUTROS_TITULO, "Notícias");
+        SimpleAdapter semFiltroAdapter = new SimpleAdapter(getActivity(), simpleAdapterData, R.layout.drawer_list_item, OUTROS_FROM, DRAWER_LIST_ITEM_TO);
+
         mNavigationMergeAdapter.addAdapter(semFiltroAdapter);
 
-        mCategoriasCursorAdapter = new SimpleCursorAdapter(getActivity(), R.layout.drawer_list_item, null, coiso_from, coiso_to, 0);
+        descricaoCategoriasConhecidas = construirDescricaoCategoriasConhecidas();
+
+        mCategoriasCursorAdapter = new SimpleCursorAdapter(getActivity(), R.layout.drawer_list_item, null, CATEGORIAS_FROM, DRAWER_LIST_ITEM_TO, 0);
         mCategoriasCursorAdapter.setViewBinder(new SimpleCursorAdapter.ViewBinder() {
             @Override
             public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
-                if (view instanceof TextView) {
-                    String categoriaOriginal = cursor.getString(columnIndex);
-                    String categoriaCapitalizada = categoriaOriginal.substring(0, 1).toUpperCase() + categoriaOriginal.substring(1);
-
-                    ((TextView) view).setText(categoriaCapitalizada);
+                String categoria = cursor.getString(cursor.getColumnIndex(NoticiaContract.Categoria.COLUMN_NAME_DESIGNACAO));
+                if (descricaoCategoriasConhecidas.containsKey(categoria)) {
+                    if (view instanceof TextView) {
+                        if (view.getId() == R.id.item_navegacao) {
+                            ((TextView) view).setText(descricaoCategoriasConhecidas.get(categoria).tituloCategoria);
+                            return true;
+                        }
+                    } else if (view instanceof ImageView) {
+                        if (view.getId() == R.id.image_view_item_navegacao) {
+                            ((ImageView) view).setImageDrawable(descricaoCategoriasConhecidas.get(categoria).iconeCategoria);
+                            return true;
+                        }
+                    }
+                } else {
+                    if (view instanceof TextView) {
+                        if (view.getId() == R.id.item_navegacao) {
+                            String categoriaCapitalizada = categoria.substring(0, 1).toUpperCase() + categoria.substring(1);
+                            ((TextView) view).setText(categoriaCapitalizada);
+                            return true;
+                        }
+                    }
                 }
-                return true;
+
+                return false;
             }
         });
         mNavigationMergeAdapter.addAdapter(mCategoriasCursorAdapter);
 
-        ArrayAdapter<String> definicoesAdapter = new ArrayAdapter<>(getActivity(), R.layout.drawer_list_item, R.id.item_navegacao, new String[]{"Definições"});
+        simpleAdapterData = new ArrayList<>(1);
+        simpleAdapterData.add(new HashMap<String, Object>(2));
+        simpleAdapterData.get(0).put(OUTROS_ICON, R.drawable.ic_settings_grey600_24dp);
+        simpleAdapterData.get(0).put(OUTROS_TITULO, "Definições");
+        SimpleAdapter definicoesAdapter = new SimpleAdapter(getActivity(), simpleAdapterData, R.layout.drawer_list_item, OUTROS_FROM, DRAWER_LIST_ITEM_TO);
+
         mNavigationMergeAdapter.addAdapter(definicoesAdapter);
 
         mCategoriasListView.setAdapter(mNavigationMergeAdapter);
@@ -104,7 +155,7 @@ public class NavigationFragment
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 if (position == 0) ((MainActivity) getActivity()).onNavigationEvent(NavigationFragment.TIPO_ON_CLICK_NOTICIAS, null);
-                else if (position == (((ListView)parent).getCount() - 1)) ((MainActivity) getActivity()).onNavigationEvent(NavigationFragment.TIPO_ON_CLICK_DEFINICOES, null);
+                else if (position == (parent.getCount() - 1)) ((MainActivity) getActivity()).onNavigationEvent(NavigationFragment.TIPO_ON_CLICK_DEFINICOES, null);
                 else ((MainActivity) getActivity()).onNavigationEvent(NavigationFragment.TIPO_ON_CLICK_CATEGORIA, String.valueOf(id));
             }
         });
@@ -144,5 +195,32 @@ public class NavigationFragment
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
         mCategoriasCursorAdapter.changeCursor(null);
+    }
+
+    private HashMap<String, DescriptorCategoriaConhecida> construirDescricaoCategoriasConhecidas
+            (){
+        Resources res = getResources();
+        String[] categorias = res.getStringArray(R.array.lista_categorias);
+        TypedArray iconesCategorias = res.obtainTypedArray(R.array.lista_icones_categorias);
+        String[] titulosCategorias = res.getStringArray(R.array.lista_titulos_categorias);
+        int quantidadeCategorias = categorias.length;
+
+        HashMap<String, DescriptorCategoriaConhecida> resultado = new HashMap<>(quantidadeCategorias);
+        for (int i = 0; i < quantidadeCategorias; i++)
+            resultado.put(categorias[i], new DescriptorCategoriaConhecida(categorias[i], iconesCategorias.getDrawable(i), titulosCategorias[i]));
+
+        return resultado;
+    }
+
+    private static class DescriptorCategoriaConhecida {
+        public DescriptorCategoriaConhecida(String categoria, Drawable iconeCategoria, String tituloCategoria){
+            this.categoria = categoria;
+            this.iconeCategoria = iconeCategoria;
+            this.tituloCategoria = tituloCategoria;
+        }
+
+        public String categoria;
+        public Drawable iconeCategoria;
+        public String tituloCategoria;
     }
 }
