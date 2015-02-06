b48340c9d290670c17df4b0ef9eadd474883dac8
Hugo Ricardo Gonçalves Martins <rikhard.martins@gmail.com>
2015-02-06 22:02:58 +0000

    Alterada a estrutura das configurações do projecto.

--- ClubeMGWeb/ClubeMGWeb/settings/base.py
+++ ClubeMGWeb/ClubeMGWeb/settings/base.py
@@ -8,21 +8,36 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+import json
 
+from django.core.exceptions import ImproperlyConfigured
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+
+SECRETS_FILE = os.path.join(os.path.dirname(__file__), "secrets.json")
+with open(SECRETS_FILE) as f:
+    secrets_file = json.loads(f.read())
+
+
+def get_secret(setting, secrets=secrets_file):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {0} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rrksxi55rso_8@gku^n375g2vbit&ga(#j=0d@o*-ck)+ziktu'
+SECRET_KEY = get_secret('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = []
 
@@ -67,9 +82,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-pt'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Lisbon'
 
 USE_I18N = True
 
